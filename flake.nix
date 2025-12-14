9b2a374c0db4b7902fe145149bd900ce7b9b0a87
WuX <wux@thenest.dev>
2025-12-14 17:27:29 +0200

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs =
+        { nixpkgs, flake-utils, ... }:
+        flake-utils.lib.eachDefaultSystem (
+            system:
+            let
+                pkgs = import nixpkgs { inherit system; };
+            in
+            {
+                devShells.default = pkgs.mkShell {
+                    shellHook = "export NIX_SHELL_NAME='charon-tree-sitter'";
+                    nativeBuildInputs = with pkgs; [
+                        tree-sitter
+                        emscripten
+                    ];
+                };
+            }
+        );
+}
